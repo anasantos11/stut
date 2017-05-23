784511e39cd798ca520ac829f41014b2f7f84245
Bruna Silva <brunasgf@gmail.com>
2017-05-22 22:37:42 -0300

    alterações feitas  no login do usuario e mudanças no cadastro

---
+++ stutBruna/js/login.js
@@ -0,0 +1,16 @@
+
+
+function showData(){
+	var idade = document.getElementById("idade").value;
+	console.log(idade);
+	if(idade == ""){}
+	else if(idade >= 18){
+		document.getElementById("Btn-Next").style.marginTop = "40px" ;
+		document.getElementById("dadosAlunoMenor").hidden = true;
+		document.getElementById("dadosAlunoMaior").hidden = false;
+	}else{
+		document.getElementById("Btn-Next").style.marginTop = "0px" ;
+		document.getElementById("dadosAlunoMaior").hidden = true;
+		document.getElementById("dadosAlunoMenor").hidden = false;
+	}
+}
